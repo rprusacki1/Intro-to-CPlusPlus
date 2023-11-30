e62717e61d3f243710638a986fec79d86a0f2245
Rachel Prusacki <111210220+rprusacki1@users.noreply.github.com>
2023-11-30 12:43:54 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// This program will read in the quantity of a particular item and its price.
+// It will then print out the total price.
+// The input will come from the keyboard and the output will go to
+// the screen.
+
+// PLACE YOUR NAME HERE
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  double b;
+  
+
+  a = 3;
+  b = (int)3.4;
+
+  cout << a << endl;
+  cout << b << endl;
+  
+
+	return 0;
+}
